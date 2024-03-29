d74e8e48273c9ee0bf5202fe6d0180de3d790dfc
Kamil Kozioł <kamilkoziol@Air-Kamil.localdomain>
2024-03-29 19:40:19 +0100

    ZZ to quit all

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,13 +8,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<leader>p", '"+p', { desc = "Paste from clipboard" })
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
+map("n", "ZZ", ":qa<CR>")
 
 map("v", "<leader>p", '"+p', { desc = "Paste from clipboard" })
 map("v", "<leader>y", '"+y', { desc = "Copy to clipboard" })
-
 map("v", ">", ">gv")
 map("v", "<", "<gv")
 
-map("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move selected down"})
-map("v", "K", ":m '>-2<CR>gv=gv", {desc = "Move selected up"})
-
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected down" })
+map("v", "K", ":m '>-2<CR>gv=gv", { desc = "Move selected up" })
